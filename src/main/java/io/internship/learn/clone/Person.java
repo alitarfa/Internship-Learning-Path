b90a879fce32ee7cfcb28d8d8f4257ec07762128
alitarfa <alitarfa123@gmail.com>
2020-04-15 14:35:48 +0200

    Clone

---
+++ src/main/java/io/internship/learn/clone/Person.java
@@ -0,0 +1,39 @@
+package io.internship.learn.clone;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person implements Cloneable{
+
+    private String name;
+    private String address;
+
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (!name.equals(person.name)) return false;
+        return address.equals(person.address);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + address.hashCode();
+        return result;
+    }
+}
