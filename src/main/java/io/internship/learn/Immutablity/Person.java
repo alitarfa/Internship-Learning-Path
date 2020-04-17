06fc628f3f20c34fb116bdd131ed0cc3ddaea79f
alitarfa <alitarfa123@gmail.com>
2020-04-17 14:46:58 +0200

    Immutablity

--- src/main/java/io/internship/learn/Immutablity/Person.java
+++ src/main/java/io/internship/learn/Immutablity/Person.java
@@ -1,11 +1,9 @@
 package io.internship.learn.Immutablity;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.ToString;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.HashMap;;
 
 @ToString
 @AllArgsConstructor
@@ -23,6 +21,15 @@ public class Person {
         return lastName;
     }
 
+
+    /**
+     * because the Hash map is a mutable class we need to provide a clone not a getter, Because with getter
+     * The Client can access to Map content and add what he wants
+     * We do the Same thing when we have other objects as attributes in the class we should always provide a copy not the
+     * a reference to the same Object ^^
+     *
+     * @return
+     */
     public HashMap<String, String> getMap() {
         return (HashMap<String, String>) map.clone();
     }
