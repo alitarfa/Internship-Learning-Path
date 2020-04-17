c592e51f264fd88e36ac1d34923c069e4d4fb231
alitarfa <alitarfa123@gmail.com>
2020-04-17 14:43:25 +0200

    Immutablity

--- src/main/java/io/internship/learn/Immutablity/Person.java
+++ src/main/java/io/internship/learn/Immutablity/Person.java
@@ -2,13 +2,29 @@ package io.internship.learn.Immutablity;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
-@Getter
+import java.util.HashMap;
+import java.util.List;
+
+@ToString
 @AllArgsConstructor
 public class Person {
 
     private final String name;
     private final String lastName;
+    private final HashMap<String, String> map;
 
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public HashMap<String, String> getMap() {
+        return (HashMap<String, String>) map.clone();
+    }
 }
 
--- src/test/java/io/internship/learn/immutabilty/Immutability.java
+++ src/test/java/io/internship/learn/immutabilty/Immutability.java
@@ -1,9 +1,10 @@
 package io.internship.learn.immutabilty;
 
+import io.internship.learn.Immutablity.ImmutablePerson;
+import io.internship.learn.Immutablity.Person;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,7 +30,6 @@ public class Immutability {
         assertEquals(1, strings.size());
 
 
-
         /**
          * as we see, we have assigned final key world to the list we assume that
          * The list will never be changed But is not the case because, the Final in this case
@@ -38,6 +38,54 @@ public class Immutability {
          */
     }
 
+
+    @Test
+    public void testUnModifiableList() {
+
+        List<String> list = Collections.unmodifiableList(Arrays.asList("One", "two"));
+
+        list.add("three");
+
+    }
+
+
+    @Test
+    public void testPersonClass() {
+
+        // So this is another way to create an immutable class using the Builder Pattern
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Test", "Test");
+
+        ImmutablePerson person = new ImmutablePerson.ImmutablePersonBuilder()
+                .setLastName("Test")
+                .setName("Test")
+                .setAddress(map)
+                .build();
+
+        System.out.println(person);
+
+        // we try to verify if this Class is immutable or no
+        HashMap<String, String> map2 = person.getAddress();
+        map2.put("Address", "Aubervillier");
+
+        assertEquals(person.getAddress().size(), 1);
+
+    }
+
+
+    /**
+     * Person is immutable class
+     */
+    @Test
+    public void testPerson() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Test", "Test");
+        Person person = new Person("Test", "Test", map);
+        person.getMap().put("Test_2", "Test_2");
+        assertEquals(person.getMap().size(), 1);
+
+    }
 
 
 }
